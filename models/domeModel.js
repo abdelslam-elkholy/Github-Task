b4f1479c13db8be51e223f343d4d224c5c99d2a5
abdelslam-elkholy <abdelslam.ahmed.elkholy@gmail.com>
2023-09-04 17:57:28 +0300

    update after kariman task

--- models/domeModel.js
+++ models/domeModel.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-const domeSchema = new mongoose.Schema(
-  {
-    domeTitle: {
-      type: String,
-      required: [true, "dome Must have Title "],
-      trim: true,
-    },
+const domeSchema = new mongoose.Schema({
+  domeTitle: {
+    type: String,
+    required: [true, "dome Must have Title "],
+    trim: true,
+  },
+
+  domeOwner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
 });
 
 const dome = mongoose.model("dome", domeSchema);
